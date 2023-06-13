280853b97fadd1723fadc363e94ba3400f421836
Taiga Morikawa <motantanmen28@gmail.com>
2023-06-13 23:19:49 +0900

    Initial Commit

---
+++ Playing-in-the-Library/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Playing-in-the-Library
+//
+//  Created by 森川大雅 on 2023/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    print("テスト")
+
+}
+
